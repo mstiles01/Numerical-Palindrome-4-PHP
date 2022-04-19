d42e73cc0a023ce9c5b5d138e382405931c92355
Michael Stiles <45317015+mstiles01@users.noreply.github.com>
2022-04-19 15:29:06 -0500

    Code wars solution

---
+++ solution.php
@@ -0,0 +1,55 @@
+<?php
+
+if(is_string($num) == true || $num < 0) {
+            return "Not valid";
+        }
+
+        $num_reverse = strrev($num);
+
+        if ($num <= 10) {
+            return 11;
+        }
+
+        if ($num == $num_reverse) {
+            return $num;
+        } else {
+
+            $adding = 0;
+            $subtract = 0;
+            $adding_done = false;
+            $num_for_adding = $num;
+            $num_for_sub = $num;
+            $adding_num = 0;
+            $sub_num = 0;
+
+
+
+                while ($num_for_adding != $num_reverse) {
+
+                    if ($num_for_adding == strrev($num_for_adding)) {
+                        $adding_done = true;
+                        $adding_num = $num_for_adding++;
+                        break;
+                    }
+                    $num_for_adding++;
+                   $adding++;
+
+                }
+
+            if ($adding_done == true) {
+                while ($num_for_sub != $num_reverse) {
+                    if ($num_for_sub == strrev($num_for_sub)) {
+                        $sub_num = $num_for_sub--;
+                        break;
+                    }
+                    $subtract++;
+                    $num_for_sub--;
+                }
+            }
+            echo $sub_num;
+            if($subtract > $adding ) {
+                return $subtract;
+            } elseif ($subtract < $adding) {
+                return $sub_num;
+            }
+        }
